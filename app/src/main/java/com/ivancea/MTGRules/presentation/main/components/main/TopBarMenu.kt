a9fb7eb6481551674bae2923b15bea150b566eee
Iván Cea Fontenla <ivancea96@outlook.com>
2023-07-26 22:46:56 +0200

    Searchbar, no suggestions

--- app/src/main/java/com/ivancea/MTGRules/presentation/main/components/main/TopBarMenu.kt
+++ app/src/main/java/com/ivancea/MTGRules/presentation/main/components/main/TopBarMenu.kt
@@ -1,17 +1,23 @@
 package com.ivancea.MTGRules.presentation.main.components.main
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.DropdownMenu
 import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ClearAll
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.DarkMode
 import androidx.compose.material.icons.filled.EmojiSymbols
 import androidx.compose.material.icons.filled.Help
@@ -19,16 +25,23 @@ import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.LightMode
 import androidx.compose.material.icons.filled.Merge
 import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Shuffle
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ivancea.MTGRules.R
 import com.ivancea.MTGRules.model.RulesSource
@@ -40,23 +53,25 @@ import java.time.format.FormatStyle
 fun TopBarMenu(darkTheme: Boolean, rulesSource: RulesSource?, onShowAbout: () -> Unit) {
     val context = LocalContext.current
 
+    var showDropdown by remember { mutableStateOf(false) }
+
     val subtitle = stringResource(R.string.action_bar_rules) + ": " + rulesSource?.date?.format(
         DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM)
     )
-
-    var showDropdown by remember { mutableStateOf(false) }
 
     TopAppBar(
         backgroundColor = MaterialTheme.colors.surface,
         title = {
             Column(verticalArrangement = Arrangement.spacedBy(4.dp)) {
-                Text(stringResource(R.string.app_name), style = MaterialTheme.typography.h6)
                 Text(
-                    subtitle, style = MaterialTheme.typography.subtitle1.merge(
-                        SpanStyle(
-                            color = MaterialTheme.colors.primary.copy(alpha = 0.7f)
-                        )
-                    )
+                    stringResource(R.string.app_name),
+                    style = MaterialTheme.typography.h6,
+                    color = MaterialTheme.colors.primary
+                )
+                Text(
+                    subtitle,
+                    style = MaterialTheme.typography.subtitle1,
+                    color = MaterialTheme.colors.primary.copy(alpha = 0.7f)
                 )
             }
         },
@@ -71,7 +86,7 @@ fun TopBarMenu(darkTheme: Boolean, rulesSource: RulesSource?, onShowAbout: () ->
             }
 
             // Search
-            // TODO: Implement search icon with search input
+            SearchBar()
 
             // More dropdown
             IconButton(onClick = { showDropdown = true }) {
@@ -174,4 +189,79 @@ fun TopBarMenu(darkTheme: Boolean, rulesSource: RulesSource?, onShowAbout: () ->
             }
         }
     )
+}
+
+@Composable
+private fun SearchBar() {
+    val context = LocalContext.current
+    val focusManager = LocalFocusManager.current
+
+    val textFieldFocusRequester = remember { FocusRequester() }
+    var searchExpanded by remember { mutableStateOf(false) }
+    var searchText by remember { mutableStateOf("") }
+
+    BackHandler(searchExpanded) {
+        searchExpanded = false
+    }
+
+    if (!searchExpanded) {
+        IconButton(onClick = { searchExpanded = true }) {
+            Icon(
+                Icons.Default.Search,
+                stringResource(R.string.menu_search),
+                tint = MaterialTheme.colors.primary
+            )
+        }
+    } else {
+        SideEffect {
+            textFieldFocusRequester.requestFocus()
+        }
+
+        TextField(
+            value = searchText,
+            singleLine = true,
+            onValueChange = {
+                searchText = it
+            },
+            trailingIcon = {
+                IconButton(onClick = { searchExpanded = false }) {
+                    Icon(
+                        Icons.Default.Close,
+                        stringResource(R.string.menu_search),
+                        tint = MaterialTheme.colors.primary
+                    )
+                }
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .focusRequester(textFieldFocusRequester),
+            label = {
+                Text(
+                    stringResource(R.string.menu_search),
+                    color = MaterialTheme.colors.primary
+                )
+            },
+            placeholder = {
+                Text(
+                    stringResource(R.string.search_hint),
+                    color = MaterialTheme.colors.primary.copy(alpha = 0.7f)
+                )
+            },
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = {
+                    focusManager.clearFocus()
+                    IntentSender.openSearch(context, searchText, null, false)
+                }
+            )
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun Preview() {
+    TopBarMenu(darkTheme = true, rulesSource = null, onShowAbout = {})
 }
