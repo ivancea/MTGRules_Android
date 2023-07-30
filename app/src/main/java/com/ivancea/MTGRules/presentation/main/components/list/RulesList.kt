a932a9cba809fffcaceda4aaace0400f64a6ba42
Iván Cea Fontenla <ivancea96@outlook.com>
2023-07-31 01:01:36 +0200

    Fixed scroll navigation rerender

--- app/src/main/java/com/ivancea/MTGRules/presentation/main/components/list/RulesList.kt
+++ app/src/main/java/com/ivancea/MTGRules/presentation/main/components/list/RulesList.kt
@@ -7,13 +7,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.text.InlineTextContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -56,15 +55,16 @@ fun RulesList(
     val lineHeight = MaterialTheme.typography.body1.lineHeight
     val textInlineContent =
         remember(context, lineHeight) { makeSymbolsMap(context, lineHeight) }
-    val listState = rememberLazyListState()
-
-    LaunchedEffect(rules, scrollToRule) {
+    val listState = remember(rules, scrollToRule) {
         if (scrollToRule != null) {
-            val index = rules.indexOfFirst { it.title == scrollToRule }
+            val firstRulesOffset = rules.indexOfFirst { it.title == scrollToRule }
 
-            if (index != -1) {
-                listState.scrollToItem(index, -10)
-            }
+            LazyListState(
+                firstRulesOffset,
+                -10
+            )
+        } else {
+            LazyListState()
         }
     }
 
