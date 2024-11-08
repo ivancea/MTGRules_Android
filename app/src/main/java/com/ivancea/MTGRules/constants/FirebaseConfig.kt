a20234dc5bceb4440453e3e6b9c7ac72ea1ebe18
Iván Cea Fontenla <ivancea96@outlook.com>
2024-11-09 00:24:46 +0100

    Moved ads keys to secrets files, and added example keys as defaults

--- app/src/main/java/com/ivancea/MTGRules/constants/FirebaseConfig.kt
+++ app/src/main/java/com/ivancea/MTGRules/constants/FirebaseConfig.kt
@@ -2,6 +2,7 @@ package com.ivancea.MTGRules.constants
 
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.ivancea.MTGRules.BuildConfig
 
 object FirebaseConfig {
     private const val ADS_ACTIVE_BY_DEFAULT = "ads_active_by_default"
@@ -12,7 +13,7 @@ object FirebaseConfig {
 
     fun getBannerAdUnitId(): String {
         return Firebase.remoteConfig.getString("banner_ad_unit_id").ifEmpty {
-            "<the-default-banner-ad-unit-id>"
+            BuildConfig.BANNER_AD_UNIT_ID
         }
     }
 }
